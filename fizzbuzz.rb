c5c68f0ce0296119e9a59471d00f5a438269979d
Sofia Jonsson <jonssonsofia93@gmail.com>
2019-02-02 22:31:04 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,36 @@
+
+
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz (int)
+  if int % 15 == 0 
+    "FizzBuzz"
+
+   elsif int % 5 == 0 
+ "Buzz"
+ 
+ 
+elsif int % 3 == 0
+  "Fizz"
+  else 
+   
+ end 
+ end
+
+
+# n = 1
+# 	while n < 101:
+# 	if n%3 == 0 and n%5 == 0:
+# 		print "fizzbuzz"
+
+# 	else if n%3 == 0:
+# 		print "fizz"
+
+# 	else if n%5 == 0:
+# 		print "buzz"
+
+# 	else:
+# 		print n
+
+# 	n += 1
+	
