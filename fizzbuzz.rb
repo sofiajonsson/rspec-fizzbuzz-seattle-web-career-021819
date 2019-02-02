bb36ee57f46c37ea0627a60f8b9dbb187cf35c7c
Sofia Jonsson <jonssonsofia93@gmail.com>
2019-02-02 21:56:51 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,21 @@
+
+
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz (int) 
+  if int % 3 == 0
+   puts "Fizz"
+   "Fizz"
+  elseif int % 5 == 0 
+    puts "Buzz"
+  elseif int % 15 == 0
+    puts "Fizzbuzz"
+ 
+  else int % 4 == 0
+  puts "nil"
+end
+# fizzbuzz(3)
+
+# fizzbuzz(5)
+
+# fizzbuzz()
