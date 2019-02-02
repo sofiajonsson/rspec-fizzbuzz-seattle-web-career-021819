d38486013cb8ef06281b4d63bfd9a9bab8a96e7d
Sofia Jonsson <jonssonsofia93@gmail.com>
2019-02-02 22:04:25 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,19 +3,31 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz (int) 
-  if int % 3 == 0
-   puts "Fizz"
-   "Fizz"
-  elseif int % 5 == 0 
-    puts "Buzz"
-  elseif int % 15 == 0
-    puts "Fizzbuzz"
+#   if int % 3 == 0
+#   puts "Fizz"
+#   "Fizz"
+#   elseif int % 5 == 0 
+#     puts "Buzz"
+#   elseif int % 15 == 0
+#     puts "Fizzbuzz"
  
-  else int % 4 == 0
-  puts "nil"
-end
-# fizzbuzz(3)
+#   else int % 4 == 0
+#   puts "nil"
+# end
 
-# fizzbuzz(5)
 
-# fizzbuzz()
+n = 1
+	while n < 101:
+	if n%3 == 0 and n%5 == 0:
+		print "fizzbuzz"
+
+	else if n%3 == 0:
+		print "fizz"
+
+	else if n%5 == 0:
+		print "buzz"
+
+	else:
+		print n
+
+	n += 1
